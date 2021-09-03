bf775e257bbfa9c4292b229c0c645e1dcc9f4836
aaron <291100854@q.com>
2021-09-03 16:17:42 +0800

    优雅关闭

--- src/main/java/com/example/parameter/verification/demo/controller/UserController.java
+++ src/main/java/com/example/parameter/verification/demo/controller/UserController.java
@@ -3,6 +3,7 @@ package com.example.parameter.verification.demo.controller;
 import com.example.parameter.verification.demo.common.result.CommonResult;
 import com.example.parameter.verification.demo.entity.dto.UserInfoDto;
 import com.example.parameter.verification.demo.entity.dto.PasswordParam;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
  * @since 2021-01-19
  */
 
+@Slf4j
 @Validated
 @RestController
 @RequestMapping("/user")
@@ -36,6 +38,7 @@ public class UserController {
     /**
      * 分组校验 group 包含 Update的 方可生效
      * 修改用户信息
+     *
      * @param userInfoDto 用户信息对象
      * @return CommonResult<String>
      */
@@ -45,5 +48,13 @@ public class UserController {
         return CommonResult.success(name);
     }
 
+
+    @GetMapping("/get")
+    CommonResult<String> get() throws InterruptedException {
+        System.out.println("=========== System.out.println =============");
+        log.info("=========== log.info ===========");
+        Thread.sleep(10000);
+        return CommonResult.success("hhh");
+    }
 
 }
